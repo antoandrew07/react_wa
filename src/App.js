6ded931ec7123f53d68732c811a944021c7b9644
antoandrew07 <andrewsmicheal49@gmail.com>
2024-01-16 12:32:23 +0530

    fst commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import './App.css'
+function App() {
+  const openWhatsAppChat = () => {
+    // Replace '123456789' with the phone number or ID of the chat you want to open
+    const phoneNumber = '8925200980';
+    
+    // Construct the WhatsApp deep link
+    const whatsappLink = `https://wa.me/${phoneNumber}`;
+    
+    // Open the WhatsApp deep link in a new tab or window
+    window.open(whatsappLink, '_blank');
+  };
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Koala🐨Tom😺</h1>
+        <button onClick={openWhatsAppChat}>Open WhatsApp Chat</button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
